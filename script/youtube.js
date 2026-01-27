911ebf4819e768895fc184f660f6805c96982cc7
chirophy <43292230+chirophy@users.noreply.github.com>
2026-01-27 13:37:20 +0800

    Add files via upload

---
+++ script/youtube.js
@@ -0,0 +1,45 @@
+async function request(method, params) {
+  return new Promise((resolve, reject) => {
+    const httpMethod = $httpClient[method.toLowerCase()];
+    httpMethod(params, (error, response, data) => {
+      resolve({ error, response, data });
+    });
+  });
+}
+
+async function main() {
+  const { error, response, data } = await request(
+    "GET",
+    "https://www.youtube.com/premium"
+  );
+
+  if (error) {
+    $done({
+      content: "Network Error",
+      backgroundColor: "",
+    });
+    return;
+  }
+
+  // 检查返回内容中是否包含“在您所在的国家/地区不可用”的英文特征字符
+  if (data.toLowerCase().includes("premium is not available") || data.toLowerCase().includes("is not currently available")) {
+    $done({
+      content: "Not Available",
+      backgroundColor: "#666666",
+    });
+    return;
+  }
+
+  $done({
+    content: "Available",
+    backgroundColor: "#FF0000", // YouTube 红色
+  });
+}
+
+(async () => {
+  main()
+    .then((_) => {})
+    .catch((error) => {
+      $done({});
+    });
+})();
